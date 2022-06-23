94d4fbde10d7c1459e9dc795e10ccbcde178e5d4
Lakshmi Soujanya Kilaru <lakshmikilaru123@gmail.com>
2022-06-23 16:51:22 +0530

    learning new ways to print

---
+++ basics/print1.py
@@ -0,0 +1,9 @@
+#to display single elements
+arr = [1,35,67,87,-123,-987,"apple","==++"]
+for i in range(0,len(arr)):
+    print(arr[i])
+for x in arr:
+    print(x)
+print(arr[6])
+print(arr[-2])
+print(arr[2:])
